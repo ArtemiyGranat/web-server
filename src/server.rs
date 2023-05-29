5a7805df9d2aa8c579a1ab8ebc1bc5ce5ed747ef
ArtemiyGranat <artemiigranat@gmail.com>
2023-05-29 23:33:48 +0400

    unified logging w & w/o feature

--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,7 @@
 #[cfg(feature = "logger")]
 use crate::logger::{LogLevel, Logger};
+#[cfg(not(feature = "logger"))]
+use crate::utils::DefaultLogger;
 use crate::{request::Request, router::Router};
 use std::{
     io::{Read, Write},
@@ -10,6 +12,8 @@ pub struct Server {
     router: Router,
     #[cfg(feature = "logger")]
     logger: Logger,
+    #[cfg(not(feature = "logger"))]
+    logger: DefaultLogger,
 }
 
 impl Server {
@@ -18,6 +22,8 @@ impl Server {
             router: Router::new(),
             #[cfg(feature = "logger")]
             logger: Logger::new(LogLevel::Debug).colored(),
+            #[cfg(not(feature = "logger"))]
+            logger: DefaultLogger::new(),
         }
     }
 
@@ -30,22 +36,15 @@ impl Server {
                 for stream in listener.incoming() {
                     match stream {
                         Ok(stream) => self.handle_connection(stream),
-                        Err(e) =>
-                        {
-                            #[cfg(feature = "logger")]
+                        Err(e) => {
                             self.logger
                                 .error(format!("Could not accept connection: {}", e));
-                            #[cfg(not(feature = "logger"))]
-                            eprintln!("Could not accept connection: {}", e);
                         }
                     }
                 }
             }
             Err(e) => {
-                #[cfg(feature = "logger")]
                 self.logger.error(format!("Could not bind a socket: {}", e));
-                #[cfg(not(feature = "logger"))]
-                eprintln!("Could not bind a socket: {}", e);
             }
         }
     }
@@ -55,32 +54,23 @@ impl Server {
         let addr = match stream.peer_addr() {
             Ok(addr) => addr.ip().to_string(),
             Err(e) => {
-                #[cfg(feature = "logger")]
                 self.logger
                     .error(format! {"Failed to detect IP address: {}", e});
-                #[cfg(not(feature = "logger"))]
-                eprintln!("Failed to detect IP address: {}", e);
                 return;
             }
         };
 
         let mut buffer = [0; 1024];
         if let Err(e) = stream.read(&mut buffer) {
-            #[cfg(feature = "logger")]
             self.logger
                 .error(format!("Could not read from stream: {}", e));
-            #[cfg(not(feature = "logger"))]
-            eprintln!("Could not read from stream: {}", e);
             return;
         }
         let raw_request = String::from_utf8_lossy(&buffer);
         let request = Request::new(&raw_request);
         if let Err(e) = request {
-            #[cfg(feature = "logger")]
             self.logger
                 .error(format!("Could not parse a request from {}: {}", addr, e));
-            #[cfg(not(feature = "logger"))]
-            eprintln!("Could not parse a request from {}: {}", addr, e);
             return;
         }
         let request = request.unwrap();
@@ -88,20 +78,14 @@ impl Server {
         self.logger.request_received(&addr, &request);
         let response = self.router.handle_request(&request);
         if let Err(e) = stream.write_all(response.to_string().as_bytes()) {
-            #[cfg(feature = "logger")]
             self.logger
                 .error(format!("Could not write to stream: {}", e));
-            #[cfg(not(feature = "logger"))]
-            eprintln!("Could not write to stream: {}", e);
             return;
         }
         #[cfg(feature = "logger")]
         self.logger.request_completed(&addr, &response);
         if let Err(e) = stream.flush() {
-            #[cfg(feature = "logger")]
             self.logger.error(format!("Could not flush stream: {}", e));
-            #[cfg(not(feature = "logger"))]
-            eprintln!("Could not flush stream: {}", e);
         }
     }
 }
