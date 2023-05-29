1429bb2b3d40b785ba97a9c095ec0c8b585b84bb
ArtemiyGranat <artemiigranat@gmail.com>
2023-05-29 23:38:30 +0400

    not using DefaultLogger if logger feature provided

--- src/utils.rs
+++ src/utils.rs
@@ -1,8 +1,10 @@
 pub const CRLF: &str = "\r\n";
 pub const FILES_PREFIX: &str = "static/";
 
+#[cfg(not(feature = "logger"))]
 pub struct DefaultLogger;
 
+#[cfg(not(feature = "logger"))]
 impl DefaultLogger {
     pub fn new() -> Self {
         Self {}
